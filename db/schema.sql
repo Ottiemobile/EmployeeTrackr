cef0f8a6b134d6f5aa57ccd9df85bb58bbe49ab9
Ethan Torres <torresethan99@gmail.com>
2020-11-16 11:02:15 -0600

    need help

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,28 @@
-DROP DATABASE IF EXISTS employees;
+DROP DATABASE IF EXISTS employee;
 
-CREATE DATABASE employees;
+CREATE DATABASE employee;
 
-USE employees;
+use employee;
 
 CREATE TABLE department (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY_KEY,
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
 );
